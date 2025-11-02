59b2ff1d6885de3e429e2ab6ff7aa0d691140753
EKL-NCI <x22353461@student.ncirl.ie>
2025-11-02 00:57:38 +0000

    :sparkles: Added Album Controller :art: Fixed formatting in album.rb :sparkles: Added create, new and index to albums_controller.rb

--- app/controllers/albums_controller.rb
+++ app/controllers/albums_controller.rb
@@ -1,3 +1,19 @@
 class AlbumsController < ApplicationController
 
+    def index
+        @album = Album.all
+    end
+
+    def new
+        @album = Album.new
+    end
+
+    def create
+        @album = Album.new(album_params)
+        if @album.save
+            redirect_to @post, notice: "Album was successfully added"
+        else
+            render :new
+        end
+    end
 end
