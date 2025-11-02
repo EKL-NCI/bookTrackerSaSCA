a5d0f4605489ca545ef0d7edb7629297beb8c240
EKL-NCI <x22353461@student.ncirl.ie>
2025-11-02 01:38:27 +0000

    :sparkles: Added Controller functionality for show, edit, update and delete

--- app/controllers/albums_controller.rb
+++ app/controllers/albums_controller.rb
@@ -16,4 +16,27 @@ class AlbumsController < ApplicationController
             render :new
         end
     end
+
+    def show
+        @album = Album.find(album_params[:id])
+    end
+
+    def edit
+        @album = Album.find(album_params[:id])
+    end
+
+    def update
+        @album = Album.find(album_params[:id])
+        if @album.update(post_album_params)
+            redirect_to @post, notice: "Album was successfully updated"
+        else
+            render :edit
+        end
+    end
+
+    def delete
+        @album = Album.find(album_params[:id])
+        @album.delete
+        redirect_to posts_url, notice: "Album was successfully deleted"
+    end
 end
